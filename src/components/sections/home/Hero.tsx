e6c7dfae4f67c47b7e043c009594ccedafa479c9
Dimitri Harding <vaughnharding@hotmail.com>
2021-05-13 03:15:50 -0500

    refactor: type-script ignore

--- src/components/sections/home/Hero.tsx
+++ src/components/sections/home/Hero.tsx
@@ -27,7 +27,9 @@ export default function Hero({
       align="center"
       justify={{ base: 'center', md: 'space-around', xl: 'space-between' }}
       direction={{ base: 'column-reverse', md: 'row' }}
-      wrap="no-wrap"
+      sx={{
+        wrap: 'no-wrap',
+      }}
       minH="70vh"
       px={8}
       mb={16}
