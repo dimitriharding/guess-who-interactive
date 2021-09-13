c83f44594bbef01219d23ee8b8641975ed303f7d
Dimitri Harding <vaughnharding@hotmail.com>
2021-09-13 00:55:22 -0500

    create error message

--- src/pages/deck/[id].tsx
+++ src/pages/deck/[id].tsx
@@ -1,16 +1,44 @@
-import { Flex, Box, Heading, useColorModeValue, Button } from '@chakra-ui/react'
-import { parseForESLint } from '@typescript-eslint/parser'
-import { useState } from 'react'
+import { Box, Heading, Button, Center, Text } from '@chakra-ui/react'
 import Carousel from '../../components/ui/Carousel'
 import { getDeckInfo } from '../api/_db'
+import Link from 'next/link'
 
 const Deck = ({ data, error }) => {
+  const renderPage = () => {
+    if (data) {
+      return (
+        <>
+          <Box h="20hv">
+            <Heading
+              marginTop={10}
+            >{`whoDat - ${data?.pageData?.title}`}</Heading>
+          </Box>
+          <Carousel
+            slides={data?.pageData?.slides ? data?.pageData?.slides : []}
+          />
+        </>
+      )
+    }
+    return (
+      <Center h="600px" flexDirection="column">
+        <Heading>Deck Not Found</Heading>
+        <Text>You can always make your own "whoDat" deck</Text>
+        <Box mt="10">
+          <Link href="/">
+            <Button variant="outline" mr="5">
+              Back to Home
+            </Button>
+          </Link>
+          <Link href="/editor">
+            <Button>Create Deck</Button>
+          </Link>
+        </Box>
+      </Center>
+    )
+  }
   return (
     <Box px={20} h="100hv">
-      <Box h="20hv">
-        <Heading marginTop={10}>{`whoDat - ${data?.pageData?.title}`}</Heading>
-      </Box>
-      <Carousel slides={data?.pageData?.slides ? data?.pageData?.slides : []} />
+      {renderPage()}
     </Box>
   )
 }
